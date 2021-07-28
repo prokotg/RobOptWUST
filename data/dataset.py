de28708ab4fcccb3c917289975d1f4f8142af56b
Tomasz Grzegorzek <tomaszgrzegorzek@outlook.com>
2021-07-28 21:49:05 +0200

    basic MNIST training with OnePixel dataset

---
+++ data/dataset.py
@@ -0,0 +1,25 @@
+import torchvision
+import numpy as np
+import warnings
+
+
+class OnePixelMNIST:
+    def __init__(self,
+                 train: bool = True,
+                 permute: bool = True,
+                 dataset: torchvision.datasets.VisionDataset = None,
+                 **kwargs):
+        super().__init__(**kwargs)
+        self.train = train
+        self.permute = permute
+        self.dataset = dataset
+        if self.train and self.permute:
+            warnings.warn('setting permute on training data has no effect')
+
+    def __getitem__(self, item):
+        image, target = self.dataset.__getitem__(item)
+        if self.train:
+            image[0, target] = 255
+        elif self.permute:
+            image[0, np.random.randint(0, 9)] = 255
+        return image, target
---
+++ train/scripts/toy_mnist.py
@@ -0,0 +1,36 @@
+import argparse
+
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+from torch.utils.data import random_split
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+from data.dataset import OnePixelMNIST
+from models.toy import MNISTToyModel
+
+parser = argparse.ArgumentParser(description='Run One-pixel MNIST training with or without gradient regularization')
+parser.add_argument('-m', '--mode', type=str, choices=['vanilla', 'bias'], default='vanilla')
+parser.add_argument('-g', '--gradient-weight', type=float)
+parser.add_argument('-p', '--permute', action='store_true')
+
+args = parser.parse_args()
+
+
+dataset = MNIST(root='../../data', train=True, download=True, transform=transforms.ToTensor())
+mnist_train, mnist_val = random_split(dataset, [55000, 5000])
+
+if args.mode == 'bias':
+    mnist_train = OnePixelMNIST(train=True, dataset=mnist_train, permute=False)
+    mnist_val = OnePixelMNIST(train=False, dataset=mnist_val, permute=args.permute)
+
+
+train_loader = DataLoader(mnist_train, batch_size=32)
+val_loader = DataLoader(mnist_val, batch_size=32)
+
+# model
+model = MNISTToyModel()
+
+# training
+trainer = pl.Trainer(max_epochs=20, precision=32, limit_train_batches=0.5)
+trainer.fit(model, train_loader, val_loader)
